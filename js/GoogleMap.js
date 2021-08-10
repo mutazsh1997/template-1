16e6e16d7fbdaeb0ac594f22c3fc0d65d2e04772
mutaz shawar <mutazshawar@hotmail.com>
2021-08-10 13:16:02 +0300

    first commit

---
+++ js/GoogleMap.js
@@ -0,0 +1,9 @@
+let map;
+let key = "AIzaSyBe8990Vv4AXbmmVkTv0dmo1nfDQehwY8w";
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 8,
+    });
+}
